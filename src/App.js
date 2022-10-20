c09e0751fd69f601598beb4a29fb8f387f81c5cb
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-10-20 07:55:43 +0330

    project initialize

---
+++ src/App.js
@@ -0,0 +1,5 @@
+const App = () => {
+    return <div>ftgby</div>
+}
+ 
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import reactDom from 'react-dom'
+import App from './App';
+
+const root = reactDom.createRoot(document.getElementById('root'));
+
+root.render(<App/>);
