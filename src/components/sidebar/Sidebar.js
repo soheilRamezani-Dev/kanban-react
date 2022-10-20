4cc8e497d5323377ccfa6a5cf817367344101670
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-10-20 19:07:44 +0330

    logo complete

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,12 +1,21 @@
 const Sidebar = () => {
-    return <div className="sidebar col-lg-2 bg-primary text-light">
-        <div className="logo">frgt</div>
-        <div className="title"></div>
-        <div className="bords-list"></div>
-        <div className="new-bord"></div>
-        <div className="nightmode"></div>
-        <div className="hide"></div>
+  return (
+    <div className="sidebar col-lg-3 bg-primary text-light">
+      <div className="logo">
+        <h1 className="p-3">
+            <span className="logo-item-0"></span>
+            <span className="logo-item-1"></span>
+            <span className="logo-item-2"></span>
+            <span className="ps-3">Kanban</span>   
+            </h1>
+      </div>
+      <div className="title"></div>
+      <div className="bords-list"></div>
+      <div className="new-bord"></div>
+      <div className="nightmode"></div>
+      <div className="hide"></div>
     </div>
-}
- 
+  );
+};
+
 export default Sidebar;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import reactDom from 'react-dom'
+import reactDom from 'react-dom/client'
 import App from './App';
 
 const root = reactDom.createRoot(document.getElementById('root'));
