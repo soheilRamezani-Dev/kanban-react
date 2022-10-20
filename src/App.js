c6f59d194a8524797d541e25f823247b05d7f24f
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-10-20 18:34:31 +0330

    sidebar template complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Sidebar from "./components/sidebar/Sidebar";
+import "./App.scss";
+
 const App = () => {
-    return <div>ftgby</div>
-}
- 
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <Sidebar />
+      </div>
+    </div>
+  );
+};
+
 export default App;
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,12 @@
+const Sidebar = () => {
+    return <div className="sidebar col-lg-2 bg-primary text-light">
+        <div className="logo">frgt</div>
+        <div className="title"></div>
+        <div className="bords-list"></div>
+        <div className="new-bord"></div>
+        <div className="nightmode"></div>
+        <div className="hide"></div>
+    </div>
+}
+ 
+export default Sidebar;
