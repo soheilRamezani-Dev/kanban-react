d81d2c8c2225adf9a69cdb69bcaebf3ee3ced8cd
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-10-20 20:06:10 +0330

    navbar complete

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,16 +1,13 @@
+import SidebarTitle from "./SidebarTitle";
+import Logo from "./Logo";
+import Navbar from "./Navbar";
+
 const Sidebar = () => {
   return (
-    <div className="sidebar col-lg-3 bg-primary text-light">
-      <div className="logo">
-        <h1 className="p-3">
-            <span className="logo-item-0"></span>
-            <span className="logo-item-1"></span>
-            <span className="logo-item-2"></span>
-            <span className="ps-3">Kanban</span>   
-            </h1>
-      </div>
-      <div className="title"></div>
-      <div className="bords-list"></div>
+    <div className="sidebar col-lg-3 bg-primary text-light p-0">
+      <Logo />
+      <SidebarTitle />
+      <Navbar />
       <div className="new-bord"></div>
       <div className="nightmode"></div>
       <div className="hide"></div>
