15a8584b87cdb1707c315ae8aa45ee7758588d7d
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-10-22 17:22:28 +0330

    Add new colum template complete

---
+++ src/components/addNewColumn/AddNewColmn.js
@@ -0,0 +1,24 @@
+import { Modal } from "react-bootstrap";
+import ModalInput from './../ModalInput'
+
+const AddNewColumn = (props) => {
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <div className="bg-primary text-white rounded">
+        <Modal.Header closeButton></Modal.Header>
+        <Modal.Body>
+          <h4 className="mb-5">Add New column</h4>
+          <ModalInput title="Column Title" id="Columntitle"/>
+          <button className="btn btn-success w-100 mt-3">Add column</button>
+        </Modal.Body>
+      </div>
+    </Modal>
+  );
+};
+
+export default AddNewColumn;
--- src/components/body/CreateNewColumnButton.js
+++ src/components/body/CreateNewColumnButton.js
@@ -1,13 +1,22 @@
+import AddNewColumn from "../addNewColumn/AddNewColmn";
+import { useState } from "react";
+
 const CreateNewColumnButton = () => {
+  const [modalShow, setModalShow] = useState(false);
+
   return (
-    <div
-      className="category-column create-column h-100 py-4 opacity-50"
-      role="button"
-    >
-      <div className="h-100 d-flex flex-column justify-content-center bg-primary mx-2 rounded my-3">
-        <h4 className="text-secondary m-0 text-center">+ New Column</h4>
+    <>
+      <div
+        className="category-column create-column h-100 py-4 opacity-50"
+        role="button"
+        onClick={()=> setModalShow(true)}
+      >
+        <div className="h-100 d-flex flex-column justify-content-center bg-primary mx-2 rounded my-3">
+          <h4 className="text-secondary m-0 text-center">+ New Column</h4>
+        </div>
       </div>
-    </div>
+      <AddNewColumn  show={modalShow} onHide={() => setModalShow(false)}/>
+    </>
   );
 };
 
